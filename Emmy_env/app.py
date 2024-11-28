a6a7c7e4ca6de42ac3c76f94bd90b35f7ac59e1c
agboremmanuel <agboremmanuelnchenge@gmail.com>
2024-11-28 17:54:10 +0100

    eroor404

--- Emmy_env/app.py
+++ Emmy_env/app.py
@@ -3,12 +3,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+#@app.route('/')
+#def home():
+    #return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
-@app.route('/')
-def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
-
+@app.route('/error')
+def trigger_error():
+    # This will raise an exception, causing a 500 error
+    raise Exception("This is a deliberate error for testing purposes.")
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
